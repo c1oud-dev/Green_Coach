c6883f50bd507e10d168874157428129d297fb62
c1oud-dev <gksmf0187@gmail.com>
2025-07-18 14:57:47 +0900

    ⭐ Initial Commit

---
+++ src/main/java/com/greencoach/GreencoachApplication.java
@@ -0,0 +1,13 @@
+package com.greencoach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreencoachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreencoachApplication.class, args);
+	}
+
+}
